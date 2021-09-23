e97eba86829030ff025ae2d2122ace912b41bb67
Madmonkey08 <ajtm082698@gmail.com>
2021-09-22 22:12:15 -0500

    App en react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import borderCollie from './media/borderCollie.jpg';
+import rhodesian from './media/rhodesian.jpg';
+import logo from './media/logo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <ul class="navbar">
+          <li>
+            <img src={logo} alt="imagen" class="logo" />
+          </li>
+          <li>
+            <button class="botonGenerico mainButton">Nuevo post</button>
+          </li>
+          <li>
+            <div class="buscar">
+              <input placeholder="Buscar una raza" />
+              <i class="fas fa-search botonGenerico iconoBusqueda"></i>
+            </div>
+          </li>
+          <li><button class="botonGenerico secondaryButton">Login</button></li>
+          <li><button class="botonGenerico mainButton">Registro</button></li>
+        </ul>
       </header>
+      <main>
+        <section>
+          <h1>Razas de Perros</h1>
+          <ul className="breedCardContainer">
+            <li class="breedCard">
+              <div className="contenedorImagen">
+                <img src={borderCollie} alt="Border Collie" />
+              </div>
+              <span className="breedTitle">Border Collie </span>
+            </li>
+            <li className="breedCard">
+              <div className="contenedorImagen">
+                <img src={rhodesian} alt="Border Collie" />
+              </div>
+              <span className="breedTitle">Rhodesian</span>
+            </li>
+          </ul>
+        </section>
+        <section></section>
+      </main>
+      <footer></footer>
     </div>
   );
 }
